49a2862153eb9bd6afdfd181bcffc231d6482357
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-10 15:37:46 +0530

    Update student_grade.py

--- student_grade.py
+++ student_grade.py
@@ -1,27 +1,24 @@
 import sys
-
-if len(sys.argv) == 6:
-    script_name = sys.argv[0]
-    marks = [float(m) for m in sys.argv[1:6]]
+if len(sys.argv) == 5:
+    marks = [float(m) for m in sys.argv[0:5]]
     print("User provided marks values:")
 else:
-    script_name = sys.argv[0]
     marks = [80, 75, 90, 85, 70]
     print("No input given - using default marks:")
+average = sum(marks) / 5
 
-average = sum(marks) / len(marks)
-
-if average >= 85:
+if average >= 90:
     grade = 'A'
-elif average >= 70:
+elif average >= 75:
     grade = 'B'
-elif average >= 55:
+elif average >= 60:
     grade = 'C'
 elif average >= 40:
     grade = 'D'
 else:
     grade = 'Fail'
 
-print(f"Marks: {marks}")
+print("\n--- Result ---")
+print("Marks:", marks)
 print(f"Average Marks: {average:.2f}")
 print(f"Grade: {grade}")
