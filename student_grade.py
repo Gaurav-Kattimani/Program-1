a9582e887ea33cde51eee2aaa0eb7f22c1070edf
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-10 15:36:11 +0530

    Update student_grade.py

--- student_grade.py
+++ student_grade.py
@@ -1,4 +1,14 @@
-marks = [85, 78, 92, 67, 74]  
+import sys
+
+if len(sys.argv) == 6:
+    script_name = sys.argv[0]
+    marks = [float(m) for m in sys.argv[1:6]]
+    print("User provided marks values:")
+else:
+    script_name = sys.argv[0]
+    marks = [80, 75, 90, 85, 70]
+    print("No input given - using default marks:")
+
 average = sum(marks) / len(marks)
 
 if average >= 85:
